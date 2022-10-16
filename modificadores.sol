268799b7443b6cb81729baca0e3bd6199d330ac2
Remix Workspace <>
2022-10-15 23:09:10 -0300

    .

--- modificadores.sol
+++ modificadores.sol
@@ -1,6 +1,52 @@
-pragma solidity >=0.4.4 <0.8.8
+pragma solidity >=0.4.4 <0.8.8;
 
-contract Modificadores {
-    // Pico pal que lee esto
+contract ModificadoresFunciones {
+
+    // Modificador view
+
+    string[] lista_estudiantes;
+
+    function nuevoAlumno(string memory _alumno) public {
+        lista_estudiantes.push(_alumno);
+    }
+
+    function getAlumno(uint _posicion) public view returns(string memory) {
+        return lista_estudiantes[_posicion];
+    }
+
+    uint x = 10;
+    function sumTox(uint _a) public view returns(uint) {
+        return x + _a;
+    }
+
     
+    // Modificador pure
+
+    function potencia(uint _a, uint _b) public pure returns(uint) {
+        return _a ** _b;
+    }
+
+    
+    // Modificador payable
+
+    mapping(address => billetera) saldoBilletera;
+
+    struct billetera {
+        string nombre;
+        address direccion;
+        uint saldo;
+    }
+
+    function pagar(string memory _nombre, uint _monto) public payable {
+        billetera memory miBilletera;
+
+        miBilletera = billetera(_nombre, msg.sender, _monto);
+        saldoBilletera[msg.sender] = miBilletera;
+    }
+
+    function verSaldo() public view returns(billetera memory) {
+        return saldoBilletera[msg.sender];
+    }
+
+
 }
